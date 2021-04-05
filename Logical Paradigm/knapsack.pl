92232a7b2382c7758d2146756ee2fbada910d563
AliCodesJava <ali.ottawa.canada@gmail.com>
2021-04-05 02:49:19 -0400

    modif prolog file reading

--- Logical Paradigm/knapsack.pl
+++ Logical Paradigm/knapsack.pl
@@ -1,17 +1,15 @@
 knapsack(Capacity, L_items_weight, L_items_value, Value, L_items_list).
 
-cleanKnapsackData(Lines, L_len, Names_L, Weights_L, Values_L, Capacity, Cleaned_Lines) 
+cleanKnapsackData(Lines, L_len, Names_L, Values_L, Weights_L, Capacity, Cleaned_Lines) 
     :- nth0(0, Lines, StrLen), normalize_space(atom(NormStrLen), StrLen), 
        atom_number(NormStrLen, L_len),
-       findall(Clean_line, cleanItemsData(Lines, Index, Clean_line), Cleaned_Lines).
-
+       findall(Clean_line, cleanItemsData(Lines, Index, Clean_line), Cleaned_Lines),
+       nth0(5, Lines, StrCapacity), normalize_space(atom(NormStrCapacity), StrCapacity),
+       atom_number(StrCapacity, Capacity).
 /*
     Pour le knapsack_1.txt
     Dans le nth0/3 ici, Index prendra les valeurs 1,2,3,4
     car Line pour Index = 0,5,6 fera raté le prédicat sublist/4
-
-    foo(BB, Index, Lines, Names_L, Weights_L, Values_L, AA) 
-    :- findall(AA, cleanItemsData(Index, Lines, N, W, Vls, AA), BB).
 */
 cleanItemsData(Lines, Index, Clean_line)
     :- nth0(Index, Lines, Line), 
@@ -21,16 +19,19 @@ cleanItemsData(Lines, Index, Clean_line)
        nth0(0, Line_L, Item_Letter),
        append([Item_Letter], Nums_Line, Clean_line).
 
-solveKnapsack(Filename, Value, L_items_list, Lines, L_len, Cleaned_Lines) 
+readKnapsackFile(Filename) 
     :- file_lines(Filename, Lines), 
-       cleanKnapsackData(Lines, L_len, Names_L, Weights_L, Values_L, Capacity, Cleaned_Lines),
-       putArr(Index, Index2, Cleaned_Lines, Value),
-       lol(I, Value, 2, 3, 2, L_items_list).
+    cleanKnapsackData(Lines, L_len, Names_L, Weights_L, Values_L, Capacity, Cleaned_Lines),
+    findall(NL,(nth0(Index,Cleaned_Lines,List),nth0(Index2,List,NL)),L),
+    put_arr(I, L, 0, 3, 0, Names_L), put_arr(I, L, 1, 3, 1, Weights_L), 
+    put_arr(I, L, 2, 3, 2, Values_L),
+    write(L_len),
+    write(Names_L),
+    write(Weights_L),
+    write(Values_L),
+    write(Capacity).
 
-putArr(Index, Index2, Cleaned_Lines, L) 
-    :- findall(NL,(nth0(Index,Cleaned_Lines,List),nth0(Index2,List,NL)),L).
-
-lol(I, L, S, D, M, RES) :- findall(E, (nth0(I, L, E), I mod D =:= M, I>=S), RES).
+put_arr(I, L, S, D, M, RES) :- findall(E, (nth0(I, L, E), I mod D =:= M, I>=S), RES).
 
 /*
     str_l_int_l/2 converti un liste de strings 
